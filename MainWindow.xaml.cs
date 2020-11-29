c513bd83e2c7a80f06c452a27dfdd3a4e82b540a
P9PAD0RV4Q2S29P\Administrator <630746464@QQ.COM>
2020-11-29 19:53:50 +0800

    创建EXCEL读取类

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using static FireExit_FlightSimulation.BinDing;
 
 namespace FireExit_FlightSimulation
@@ -21,8 +11,11 @@ namespace FireExit_FlightSimulation
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         Source Data_S_source = new Source();
         Source Data_R_source = new Source();
+        string filepath;
+        Excel_NPIO EXCEL=new Excel_NPIO();
         public MainWindow()
         {
             InitializeComponent();
@@ -33,8 +26,21 @@ namespace FireExit_FlightSimulation
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Data_S_source.Data_String += "12";
-            Data_R_source.Data_String += "34";
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            //SaveFileDialog fileDialog = new SaveFileDialog();
+            //FolderBrowserDialog fileDialog = new FolderBrowserDialog();
+            //fileDialog.Multiselect = true;//允许多选
+            fileDialog.RestoreDirectory = false;//使用上次打开目录
+            fileDialog.Title = "请选择文件";
+            fileDialog.Filter = "Excel(*.xlsx)|*.xlsx;*.xls";
+            if (fileDialog.ShowDialog()==true)
+            {
+                filepath = fileDialog.FileName;
+            }
+            EXCEL.Open_Create_WorkBook(filepath);
+            EXCEL.Open_WorkSheet(0);
+            Console.WriteLine(new int[2] { EXCEL.Read_WorkSheet().Count, EXCEL.Read_WorkSheet()[0].Count });
         }
     }
 }
