32c70f82299513cd730c83e22bab2c8849da7201
P9PAD0RV4Q2S29P\Administrator <630746464@QQ.COM>
2020-12-11 16:47:21 +0800

    增加TCP类，Client功能验证完成

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,14 +13,19 @@ namespace FireExit_FlightSimulation
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
+  
     public partial class MainWindow : Window
     {
-        
+        Queue q = new Queue();//创建消息队列
+        public class Element//含命令和数据的类
+        {
+            public string command;
+            public object data;
+        }
+
         static Source Data_S_source = new Source();
         static Source Data_R_source = new Source();
         string filepath;
-
-       UDPHelper UDP_CONNECT=new UDPHelper();
         public MainWindow()
         {
             InitializeComponent();
@@ -26,12 +33,12 @@ namespace FireExit_FlightSimulation
             Bind(Data_S_source, Data_S, TextBox.TextProperty, "String");
             Bind(Data_R_source, Data_R, TextBlock.TextProperty, "String");
 
-            ThreadStart childref = new ThreadStart(UDP_Receview);
+            ThreadStart childref = new ThreadStart(Machine);
             Thread childThread = new Thread(childref);
             childThread.Start();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Load_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog fileDialog = new OpenFileDialog();
             //SaveFileDialog fileDialog = new SaveFileDialog();
@@ -53,32 +60,77 @@ namespace FireExit_FlightSimulation
             EXCEL.File_SaveAs(filepath);
             EXCEL.File_Close();
         }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Open_Click(object sender, RoutedEventArgs e)
         {
-            UDP_CONNECT.UDP_Open("192.168.1.247", 51901);
+           Element a=new Element();
+            a.command = "打开通讯";
+            a.data = null;
+            q.Enqueue(a);
         }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void Send_Click(object sender, RoutedEventArgs e)
         {
-            UDP_CONNECT.UDP_Write(Data_S_source.Data_String, "255.255.255.255", 51900);
+            Element a = new Element();
+            a.command = "写入数据";
+            a.data = Data_S_source.Data_String;
+            q.Enqueue(a);
         }
-
-        private void UDP_Receview()
+        private void Window_Close(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            string data;
-            string ip;
-            int port;
-            while (true)
+            Element a = new Element();
+            a.command = "退出";
+            a.data = Data_S_source.Data_String;
+            q.Enqueue(a);
+        }
+        private void Machine()
+        {
+            Client TCP_Client = new Client();
+            HandleAndStream hs =null;
+            bool run = true;
+            while (run)
             {
-               UDP_CONNECT.UDP_Read( out data,out ip,out port);
-                if (data != "")
+                if (q.Count > 0)
                 {
-                    Data_R_source.Data_String += ip +":" +port.ToString() + "：" + data+"\n";
+                    Element element = (Element)q.Dequeue();
+                    switch (element.command)
+                    {
+                        case "打开通讯":
+                            try
+                            {
+                                /*如果固定本地端口，服务器里已经保存此端口，重复开关客户端，会报错！
+                                 如果使用不固定端口，则每次使用一个不同的端口号，不会与服务器保存的重复，不报错！*/
+                                hs = TCP_Client.TCP_Connect("192.168.0.120", 51900, 0);
+                                Data_R_source.Data_String += "通讯端口打开成功！" + "\n";
+                            }
+                            catch(Exception e)
+                            {
+                                Data_R_source.Data_String += "通讯端口打开失败！" + "\n"+e.Message+ "\n";
+                            }
+                            break;
+                        case "写入数据":
+                            TCP_Client.TCP_Write(hs, (string)element.data);
+                            break;
+                        case "退出":
+                            TCP_Client.TCP_Close(hs, null);
+                            run = false;
+                            break;
+                        default://默认读取数据
+                            
+                            break;
+                    }
+                }
+                else
+                {
+                    string a = TCP_Client.TCP_Read(hs, 1024, 10);
+                    if (a.Length > 0) 
+                    {
+                        Data_R_source.Data_String +=a+"\n";
+                    }
                 }
                 Thread.Sleep(100);
             }
         }
+
+       
     }
 
 }
---
+++ TCPHelper.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace FireExit_FlightSimulation
+{
+    public class HandleAndStream//创建一个公共的TCP句柄和数据流类
+    {
+        public TcpClient handle;
+        public NetworkStream stream;
+    }
+    class TCPHelper
+    {
+        public void TCP_Write(HandleAndStream hs, string message)//发送数据
+        {
+            Byte[] data_Send = Encoding.Default.GetBytes(message);
+            int data_length = data_Send.Length;
+            hs.stream.Write(data_Send,0, data_length);
+        }
+        public string TCP_Read(HandleAndStream hs, int lenght,int time_out)//接收数据
+        {
+            string message = "";
+            int i = 0;
+            bool read = true;
+            while (read)
+            {
+                if (hs != null)
+                {
+                    //利用Available属性可以使阻塞式IO当做不阻塞使用
+                    if (hs.handle.Available >= lenght)//如果数据长度满足则停止读取
+                    {
+                        Byte[] data = new Byte[lenght];
+                        Int32 bytes = hs.stream.Read(data, 0, data.Length);
+                        message = Encoding.Default.GetString(data, 0, bytes);
+                        read = false;
+                    }
+                    else if (i >= time_out)//如果超时，则将现有的数据读出，停止读取
+                    {
+                        if (hs.handle.Available > 0)
+                        {
+                            Byte[] data = new Byte[lenght];
+                            Int32 bytes = hs.stream.Read(data, 0, data.Length);
+                            message = Encoding.Default.GetString(data, 0, bytes);
+                        }
+                        read = false;
+                    }
+                    else
+                    {
+                        message = "";
+                    }
+                }
+                else
+                {
+                    message = "";
+                    read = false;
+                }
+                i++;
+                Thread.Sleep(1);//延时1毫秒
+         }
+            return message;
+        }
+        public void TCP_Close(HandleAndStream hs ,  TcpListener Listener_Handle)//关闭TCP句柄
+        {
+            if (hs != null)
+            {
+                hs.stream.Close();
+                hs.handle.Close();
+            }
+            if (Listener_Handle != null)
+            {
+                Listener_Handle.Stop();
+            }
+        }
+    }
+    class Client: TCPHelper
+    {
+        public HandleAndStream TCP_Connect(string ip_remote, int port_remote, int port_local)//创建客户端连接
+        {
+            HandleAndStream hs = new HandleAndStream();
+
+            IPEndPoint Clinet_EndPoint = new IPEndPoint(IPAddress.Any, port_local);//指定本地端口号
+            TcpClient Connect_Handle = new TcpClient(Clinet_EndPoint);//重新实例化客户端
+            Connect_Handle.Connect(ip_remote, port_remote);//绑定远程端口
+            hs.handle = Connect_Handle;
+            hs.stream = Connect_Handle.GetStream();
+            return hs;
+        }
+
+    }
+    class Server : TCPHelper
+    {
+        public void TCP_Listener_Create(string ip, int port)//创建TCP侦听器
+        {
+            TcpListener Listener_Handle = new TcpListener(IPAddress.Parse(ip), port);//绑定本地的IP（多个网卡中的某一个）和尝试连接的远程端口号
+            Listener_Handle.Start();
+        }
+        public TcpClient TCP_Listener_Wait(TcpListener Listener_Handle, int time_out)
+        {
+            TcpClient client = null;
+            int i = 0;
+            bool wait = true;
+            while (wait)
+            {
+                if (Listener_Handle.Pending())//侦听器正在挂起，无客户端接入
+                {
+                    if (i >= time_out)//等待已超时
+                    {
+                        wait = false;
+                    }
+                }
+                else//有客户端接入
+                {
+                    client = Listener_Handle.AcceptTcpClient();
+                    wait = false;
+                }
+                i++;
+                Thread.Sleep(1);
+            }
+            return client;
+        }//等待客户端接入
+    }
+}
